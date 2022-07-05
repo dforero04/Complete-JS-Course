4b842739f84c3b9245bf809120dbbe60fe02573d
dforero04 <dxforero@ualr.edu>
2022-07-05 17:15:52 -0500

    almost done with section

--- Advanced DOM and Events/script.js
+++ Advanced DOM and Events/script.js
@@ -3,35 +3,35 @@
 /////////////////////////////////////////////////
 // Selecting Elements
 /////////////////////////////////////////////////
-const header = document.querySelector('.header');
+// const header = document.querySelector('.header');
 
 // returns a NodeList
-const allSections = document.querySelectorAll('.section');
-console.log(allSections);
+// const allSections = document.querySelectorAll('.section');
+// console.log(allSections);
 
 // returns HTMLCollection
 // - this return type is a "live" collection
 // - immediately updates when DOM is updates
-const allButtons = document.getElementsByTagName('button');
-console.log(allButtons);
-console.log(document.getElementsByClassName('btn'));
+// const allButtons = document.getElementsByTagName('button');
+// console.log(allButtons);
+// console.log(document.getElementsByClassName('btn'));
 
 /////////////////////////////////////////////////
 // Creating and Inserting Elements
 /////////////////////////////////////////////////
 
 // Returns a DOM Element
-const message = document.createElement('div');
-message.classList.add('cookie-message');
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
 // you are able to add HTML elements using .innerHTML
-message.innerHTML =
-  'We use cookies for improved analytics.<button class="btn btn--close-cookie">Got it!</button>';
+// message.innerHTML =
+// 'We use cookies for improved analytics.<button class="btn btn--close-cookie">Got it!</button>';
 
 // - Prepend adds as first child to parents element
 // header.prepend(message);
 // - Append adds as last child to parents
 // - This element will only appear once
-header.append(message);
+// header.append(message);
 // - This is how you place in multiple places
 // header.append(message.cloneNode(true));
 
@@ -41,22 +41,22 @@ header.append(message);
 /////////////////////////////////////////////////
 // Delete Elements
 /////////////////////////////////////////////////
-document.querySelector('.btn--close-cookie').addEventListener('click', () => {
-  message.remove();
-});
+// document.querySelector('.btn--close-cookie').addEventListener('click', () => {
+// message.remove();
+// });
 
 /////////////////////////////////////////////////
 // Styling Elements
 /////////////////////////////////////////////////
 
 // This way only changes inline styles
-message.style.backgroundColor = '#37383d';
-message.style.width = '106%';
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '106%';
 
 // getComputedStyle will get all styles on an element
-console.log(getComputedStyle(message).height);
-message.style.height =
-  Number.parseFloat(getComputedStyle(message).height, 10) + 10 + 'px';
+// console.log(getComputedStyle(message).height);
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height, 10) + 10 + 'px';
 
 // This is how you change custom style properties in the CSS file
 // document.documentElement.style.setProperty('--color-primary', 'blue');
@@ -66,19 +66,170 @@ message.style.height =
 /////////////////////////////////////////////////
 
 // You are only able to read standard element attributes, not custom ones
-const logo = document.querySelector('.nav__logo');
-console.log(logo.alt);
-console.log(logo.src);
-console.log(logo.className);
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.src);
+// console.log(logo.className);
 
-// For non-standard attributes
-console.log(logo.getAttribute('designer'));
+// // For non-standard attributes
+// console.log(logo.getAttribute('designer'));
 
-// Setting attributes
-logo.alt = 'Beautiful minimalist logo';
-console.log(logo.alt);
-logo.setAttribute('company', 'Bankist');
+// // Setting attributes
+// logo.alt = 'Beautiful minimalist logo';
+// console.log(logo.alt);
+// logo.setAttribute('company', 'Bankist');
 
 // Data Attributes
 // These are custom attributes within the dataset object
-console.log(logo.dataset.versionNumber);
+// console.log(logo.dataset.versionNumber);
+
+/////////////////////////////////////////////////
+// Smooth Scrolling
+/////////////////////////////////////////////////
+// const btnScrollTo = document.querySelector('.btn--scroll-to');
+// const section1 = document.querySelector('#section--1');
+
+// btnScrollTo.addEventListener('click', function (e) {
+//   // Returns coords based on the element
+//   const s1coords = section1.getBoundingClientRect();
+
+//   // Shows the current scroll position
+//   console.log(window.scrollX);
+//   console.log(window.scrollY);
+
+//   // This is an old way to do it by calculating the appropriate value
+//   // window.scrollTo({
+//   //   left: s1coords.left + window.pageXOffset,
+//   //   top: s1coords.top + window.pageYOffset,
+//   //   behavior: 'smooth',
+//   // });
+
+//   // More modern way to do it
+//   section1.scrollIntoView({
+//     behavior: 'smooth',
+//   });
+// });
+
+/////////////////////////////////////////////////
+// Types of Events and Event Handlers
+// - all events and info can be found here
+// https://developer.mozilla.org/en-US/docs/Web/Events
+/////////////////////////////////////////////////
+
+// const h1 = document.querySelector('h1');
+// // AddEventListener is mainly used because you can add multiple event listeners to the same event
+// // Also, you can remove an event listener in case you don't need it anymore
+
+// const alertH1 = function (e) {
+//   alert('addEventListener: You are reading the heading!');
+
+//   h1.removeEventListener('mouseenter', alertH1);
+// };
+// // -'mouseenter' is triggered when the mouse enters the elements area
+// h1.addEventListener('mouseenter', alertH1);
+
+/////////////////////////////////////////////////
+// Event Propogation: Bubbling and Capturing
+// - Event Propogation goes through 3 "phases"
+//  -- Capturing Phase - the event travels down DOM tree to target element
+//  -- Target Phase - the event reaches its target and performs the callback function
+//  -- Bubbling Phase - go back up the DOM tree to the root element (document)
+// - This is important to know because it is as if the event happened in all of
+//  the parent elements as well
+/////////////////////////////////////////////////
+
+// - This example shows that the background color of all parent nodes change when
+// the lowest link is clicked
+// - Also, when each level is clicked individually, the e.target shows which
+// specific element the event was called on, while the e.currentTarget shows
+// which element the current event handler is attached (which is basically the this keyword).
+
+// // Example
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (max - min + 1) + min);
+// const randomColor = () =>
+//   `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+// // addEventListener only gets events from the bubbling phase
+// //  -- However, you can add a 3rd parameter (boolean) to tell it to work during the capturing phase
+// //  -- Capturing is rarely used in modern development
+// document.querySelector('.nav__link').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log('Link:', e.target);
+//   console.log('Link:', e.currentTarget);
+
+//   // Stop propogation
+//   // This stops propogation at the current event level
+//   // e.stopPropagation();
+// });
+// document.querySelector('.nav__links').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log('Container:', e.target);
+//   console.log('Container:', e.currentTarget);
+// });
+// document.querySelector('.nav').addEventListener(
+//   'click',
+//   function (e) {
+//     this.style.backgroundColor = randomColor();
+//     console.log('Nav:', e.target);
+//     console.log('Nav:', e.currentTarget);
+//   },
+//   true
+// );
+
+/////////////////////////////////////////////////
+// Event Delegation
+/////////////////////////////////////////////////
+
+// // This is the way you would implement an event handler on each link WITHOUT event delegation
+// // This is adding individual event listeners to every single child element
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+// // With Event Delegation, you can add an event listener to a common parent element
+// // of all child elements.
+// // Then determine what element originated the event.
+// document.querySelector('.nav__links').addEventListener('click', function (e) {
+//   e.preventDefault();
+//   // Matching strategy
+//   // This is used to ignore clicks that happen outside of the desired elements
+//   if (e.target.classList.contains('nav__link')) {
+//     const id = e.target.getAttribute('href');
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   }
+// });
+
+/////////////////////////////////////////////////
+// DOM Traversing
+/////////////////////////////////////////////////
+
+// const h1 = document.querySelector('h1');
+
+// // going down to child
+// console.log(h1.querySelectorAll('.highlight'));
+// console.log(h1.childNodes);
+// console.log(h1.children);
+// h1.firstElementChild.style.color = 'white';
+// h1.lastElementChild.style.color = 'orangered';
+
+// // going up to parents
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
+
+// //  -- closest is similar to querySelector, but finds parents
+// h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+// // going sideways to access siblings
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
+
+// console.log(h1.previousSibling);
+// console.log(h1.nextSibling);
+
+// // access all siblings
+// console.log(h1.parentElement.children);
--- Bankist Homepage/script.js
+++ Bankist Homepage/script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -29,3 +31,81 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+
+  section1.scrollIntoView({
+    behavior: 'smooth',
+  });
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+// Tabbed Component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const clicked = e.target.closest('.operations__tab');
+  // Guard clause
+  if (!clicked) return;
+
+  tabs.forEach(tab => {
+    tab.classList.contains(`operations__tab--${clicked.dataset.tab}`)
+      ? tab.classList.add('operations__tab--active')
+      : tab.classList.remove('operations__tab--active');
+  });
+
+  tabsContent.forEach(content => {
+    content.classList.contains(`operations__content--${clicked.dataset.tab}`)
+      ? content.classList.add('operations__content--active')
+      : content.classList.remove('operations__content--active');
+  });
+});
+
+// Menu Fade Animation
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+const appBar = document.querySelector('.nav');
+appBar.addEventListener('mouseover', handleHover.bind(0.5));
+appBar.addEventListener('mouseout', handleHover.bind(1));
+
+// Sticky Navigation
+// TODO: Research IntersectionObserver more
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  !entry.isIntersecting
+    ? appBar.classList.add('sticky')
+    : appBar.classList.remove('sticky');
+};
+
+const header = document.querySelector('.header');
+const appBarHeight = appBar.getBoundingClientRect().height;
+
+const headerObs = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${appBarHeight}px`,
+});
+headerObs.observe(header);
