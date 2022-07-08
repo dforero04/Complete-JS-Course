3e169806d880f31579b1f9e3813efb4e0a89c539
dforero04 <dxforero@ualr.edu>
2022-07-08 17:15:49 -0500

    almost done with oop section

--- Object Oriented Programming/script.js
+++ Object Oriented Programming/script.js
@@ -59,7 +59,7 @@
 // // Coding Challenge #1
 // /////////////////////////////////////////////////
 
-// // Your tasks:
+// Your tasks:
 
 // // 1. Use a constructor function to implement a 'Car'. A car has a 'make' and a
 // // 'speed' property. The 'speed' property is the current speed of the car in
@@ -211,47 +211,187 @@
 // lou.init('Lou', 1961);
 // lou.calcAge();
 
-/////////////////////////////////////////////////
-// Coding Challenge #2
-/////////////////////////////////////////////////
-//Your tasks:
+// /////////////////////////////////////////////////
+// // Coding Challenge #2
+// /////////////////////////////////////////////////
+// //Your tasks:
 
-//1. Re-create Challenge #1, but this time using an ES6 class (call it 'CarCl')
-class CarCl {
-  constructor(make, speed) {
-    this.make = make;
-    this.speed = speed;
-  }
+// //1. Re-create Challenge #1, but this time using an ES6 class (call it 'CarCl')
+// class CarCl {
+//   constructor(make, speed) {
+//     this.make = make;
+//     this.speed = speed;
+//   }
 
-  accelerate() {
-    this.speed += 10;
-    console.log(`${this.make} accelerated to ${this.speed} mph`);
-  }
+//   accelerate() {
+//     this.speed += 10;
+//     console.log(`${this.make} accelerated to ${this.speed} mph`);
+//   }
 
-  brake() {
-    this.speed -= 5;
-    console.log(`${this.make} braked to ${this.speed} mph`);
-  }
+//   brake() {
+//     this.speed -= 5;
+//     console.log(`${this.make} braked to ${this.speed} mph`);
+//   }
 
-  get speedUK() {
-    return `${this.make}'s speed in UK is ${this.speed * 1.6}`;
-  }
+//   get speedUK() {
+//     return `${this.make}'s speed in UK is ${this.speed * 1.6}`;
+//   }
 
-  set speedUK(speed) {
-    this.speed = speed * 1.6;
-  }
-}
-const yourCar = new CarCl('Honda', 40);
-yourCar.accelerate();
-yourCar.accelerate();
-yourCar.brake();
+//   set speedUK(speed) {
+//     this.speed = speed * 1.6;
+//   }
+// }
+// const yourCar = new CarCl('Honda', 40);
+// yourCar.accelerate();
+// yourCar.accelerate();
+// yourCar.brake();
 
-// 2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide
-// by 1.6)
-console.log(yourCar.speedUK);
+// // 2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide
+// // by 1.6)
+// console.log(yourCar.speedUK);
 
-// 3. Add a setter called 'speedUS' which sets the current speed in mi/h (but
-// converts it to km/h before storing the value, by multiplying the input by 1.6)
-yourCar.speedUK = 10;
-console.log(yourCar.speed);
-console.log('works');
+// // 3. Add a setter called 'speedUS' which sets the current speed in mi/h (but
+// // converts it to km/h before storing the value, by multiplying the input by 1.6)
+// yourCar.speedUK = 10;
+// console.log(yourCar.speed);
+
+// /////////////////////////////////////////////////
+// // Inheritance Between "Classes": Constructor Functions
+// /////////////////////////////////////////////////
+
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+// };
+
+// Person.prototype.calcAge = function () {
+//   console.log(new Date().getFullYear() - this.birthYear);
+// };
+
+// const Student = function (firstName, birthYear, course) {
+//   Person.call(this, firstName, birthYear);
+//   this.course = course;
+// };
+
+// // Must be done before adding anything to Student.prototype because
+// // it would override.
+// Student.prototype = Object.create(Person.prototype);
+// Student.prototype.constructor = Student;
+
+// Student.prototype.introduce = function () {
+//   console.log(`My name is ${this.firstName} and I am taking ${this.course}`);
+// };
+
+// const student1 = new Student('Daniel', 1992, 'CS101');
+// student1.introduce();
+// student1.calcAge();
+// console.log(student1);
+
+// /////////////////////////////////////////////////
+// // Coding Challenge #3
+// /////////////////////////////////////////////////
+
+// // Your tasks:
+// // 1. Use a constructor function to implement an Electric Car (called 'EV') as a child
+// // "class" of 'Car'. Besides a make and current speed, the 'EV' also has the
+// // current battery charge in % ('charge' property)
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
+// Car.prototype.accelerate = function () {
+//   this.speed += 10;
+//   console.log(`${this.make} accelerated to ${this.speed}`);
+// };
+// Car.prototype.brake = function () {
+//   this.speed -= 5;
+//   console.log(`${this.make} braked to ${this.speed}`);
+// };
+
+// const ElectricCar = function (make, speed, charge) {
+//   Car.call(this, make, speed);
+//   this.charge = charge;
+// };
+
+// // 2. Implement a 'chargeBattery' method which takes an argument
+// // 'chargeTo' and sets the battery charge to 'chargeTo'
+// ElectricCar.prototype = Object.create(Car.prototype);
+// ElectricCar.prototype.constructor = ElectricCar;
+
+// ElectricCar.prototype.chargeBattery = function (chargeTo) {
+//   this.charge = chargeTo;
+// };
+// // 3. Implement an 'accelerate' method that will increase the car's speed by 20,
+// // and decrease the charge by 1%.
+// ElectricCar.prototype.accelerate = function () {
+//   this.speed += 20;
+//   this.charge--;
+//   console.log(
+//     `${this.make} accelerated to ${this.speed}, with charge of ${this.charge}`
+//   );
+// };
+
+// // 4. Create an electric car object and experiment with calling 'accelerate',
+// // 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when
+// // you 'accelerate'! Hint: Review the definiton of polymorphism 😉
+// const tesla = new ElectricCar('Tesla', 120, 23);
+// console.log(tesla);
+// tesla.chargeBattery(75);
+// console.log(tesla);
+// tesla.accelerate();
+// console.log(tesla);
+
+// /////////////////////////////////////////////////
+// // Inheritance Between "Classes": ES6 Classes
+// /////////////////////////////////////////////////
+
+// class PersonClass {
+//   constructor(firstName, birthYear) {
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+//   }
+
+//   calcAge() {
+//     const now = new Date();
+//     console.log(now.getFullYear() - this.birthYear);
+//   }
+
+//   greet() {
+//     console.log(`Hey, my name is ${this.firstName}`);
+//   }
+
+//   get age() {
+//     return new Date().getFullYear() - this.birthYear;
+//   }
+
+//   set firstName(name) {
+//     this._firstName = name;
+//   }
+
+//   get firstName() {
+//     return this._firstName;
+//   }
+
+//   static hey() {
+//     console.log('Hey there!');
+//   }
+// }
+
+// class StudentClass extends PersonClass {
+//   constructor(firstName, birthYear, course) {
+//     // This super() call must have first
+//     super(firstName, birthYear);
+//     this.course = course;
+//   }
+//   introduce() {
+//     console.log(`My name is ${this.firstName} and I am taking ${this.course}`);
+//   }
+//   calcAge() {
+//     console.log(`I am ${this.age} years old.`);
+//   }
+// }
+
+// const studentDan = new StudentClass('Dan', 2010, 'CS301');
+// studentDan.introduce();
+// studentDan.calcAge();
+// console.log(studentDan);
